623640556676a5d79099c1bd32edb66f2a2da4bf
Hack Reactor Student <student@hackreactor.com>
2017-08-17 11:43:11 -0700

    Added drag and drop componenet

---
+++ client/components/App.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import Dropzone from 'react-dropzone';
+
+export default class App extends Component {
+    constructor() {
+        super();
+        this.state = { files: [] };
+        this.onDrop = this.onDrop.bind(this);
+    }
+    onDrop(files) {
+        this.setState({ 
+            files
+        });
+    }
+
+    render() {
+        return (
+            <section>
+                <div>
+                    <Dropzone onDrop={this.onDrop}>
+                        <p>Try dropping some files here or click files to upload</p>
+                    </Dropzone>
+                </div>
+                <aside>
+                    <h2>Dropped files</h2>
+                    <ul>
+                        {
+                            this.state.files.map(f => <li>{f.name} - {f.size} bytes</li>)
+                        }
+                    </ul>
+                </aside>
+            </section>
+        )
+    }
+}
